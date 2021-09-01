f9c1e4596a591795c66d9c0bef1c55cdb0fc1bb9
Nengyi Jonathan Jiang <k1334755@students.katyisd.org>
2021-09-01 11:53:19 -0500

    commit

--- TrianglePaint.java
+++ TrianglePaint.java
@@ -7,12 +7,10 @@ public class TrianglePaint extends JPanel implements MyMouseListener.MouseObserv
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
 
-    private enum States{
-        IDLE, DRAWING, ERASING
-    };
+    private enum States{IDLE, DRAWING, ERASING}
     private States currState = States.IDLE;
 
-    private static double time = 0;
+    private double time = 0;
 
     private static final double SQRT_3_2 = 0.86602540378;
     private static final int SIZE = 64;
@@ -25,19 +23,19 @@ public class TrianglePaint extends JPanel implements MyMouseListener.MouseObserv
         new Color(100,130,170),
         new Color( 90,  0,  0),
         new Color(222,127,127),
-        new Color(135,245, 66),
+        new Color(175,227,141),
         new Color( 66,135, 33),
     };
 
     public MyKeyListener keys;
 
-    private int[][] data = new int[SIZE][SIZE * 2];
+    private int[][] triangleData = new int[SIZE][SIZE * 2];
+    private int[][] edgeData = new int[SIZE][SIZE * 3];
 
     private double scale = 33.3;
 
-    private int mouseX = 0;
-    private int mouseY = 0;
-    public int currColor = 0;
+    private int lastMouseX, lastMouseY, mouseX = 0, mouseY;
+    private int currColor;
 
     public TrianglePaint() // constructor - sets up the class
     {
@@ -63,20 +61,24 @@ public class TrianglePaint extends JPanel implements MyMouseListener.MouseObserv
         Graphics2D g = (Graphics2D)window;
         g.setStroke(new BasicStroke(1));
         
+        boolean blinking = ((int)(time * 15) & 3) != 0;
+
+        //Draw filled triangles (or triangle outlines)
         for(int i = 0; i < SIZE; i++){
             int x1 = (int)((i - 1.) * scale), x2 = (int)((i - .5) * scale),
                 x3 = (int)((i + 0.) * scale), x4 = (int)((i + .5) * scale),
                 x5 = (int)((i + 1.) * scale);
-            
             for(int j = 0; j < SIZE * 2; j++){
                 int y1 = (int)((j / 2) * scale * SQRT_3_2), y2 = (int)((j / 2 + 1) * scale * SQRT_3_2);
 
-                if(i == mouseX && j == mouseY && ((int)(time * 15) & 1) == 0){ //cell is hovered over
+                if(i == mouseX && j == mouseY && blinking){ //cell is hovered over
                     g.setColor(COLORS[this.currColor]);
                     fillTriAt(g,x1,x2,x3,x4,x5,y1,y2,j);
+                    g.setColor(new Color(200,200,200));
+                    drawTriAt(g,x1,x2,x3,x4,x5,y1,y2,j);
                 }
-                else if(data[i][j] > 0){   //cell has color
-                    g.setColor(COLORS[data[i][j] - 1]);
+                else if(triangleData[i][j] > 0){   //cell has color
+                    g.setColor(COLORS[triangleData[i][j] - 1]);
                     fillTriAt(g,x1,x2,x3,x4,x5,y1,y2,j);
                 }
                 else if(!keys.isKeyPressed(KeyEvent.VK_V)){ //cell is empty, and user not pressing 'v'
@@ -128,16 +130,19 @@ public class TrianglePaint extends JPanel implements MyMouseListener.MouseObserv
     @Override
     public void onMouseMove(int x, int y) {
         double Y = x / scale / SQRT_3_2, X = y / scale - 0.5 * (Y % 2) + 1.;
+        lastMouseX = mouseX;
+        lastMouseY = mouseY;
+
         mouseX = (int)X;
         mouseY = 2 * (int)Y + (int)((X % 1.0) + (Y % 1.0));
 
         switch(currState){
             case IDLE: break;
             case DRAWING:
-                data[mouseX][mouseY] = currColor + 1;
+                triangleData[mouseX][mouseY] = currColor + 1;
                 break;
             case ERASING:
-                data[mouseX][mouseY] = 0;
+                triangleData[mouseX][mouseY] = 0;
                 break;
         }
     }
@@ -154,12 +159,14 @@ public class TrianglePaint extends JPanel implements MyMouseListener.MouseObserv
         switch(b){
             case LEFT_CLICK:
                 currState = States.DRAWING;
-                data[mouseX][mouseY] = currColor + 1;
+                onMouseMove(x,y);
+                //triangleData[mouseX][mouseY] = currColor + 1;
                 break;
             case MIDDLE_CLICK: break;
             case RIGHT_CLICK:
                 currState = States.ERASING;
-                data[mouseX][mouseY] = 0;
+                onMouseMove(x,y);
+                //triangleData[mouseX][mouseY] = 0;
                 break;
             case NO_CLICK: break;
             
